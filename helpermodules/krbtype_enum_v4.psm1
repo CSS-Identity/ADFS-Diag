8db11f5a44cef73eec0357c09f120a4b335c334b
ChristianWeisse <56339271+ChristianWeisse@users.noreply.github.com>
2021-12-22 13:27:11 +0100

    KRB Module update
    minor update so the module now supports all  documented etype flags

--- helpermodules/krbtype_enum_v4.psm1
+++ helpermodules/krbtype_enum_v4.psm1
@@ -1,5 +1,6 @@
-﻿#HelperModule for Kerberos EncryptionType enumerations
+#HelperModule for Kerberos EncryptionType enumerations
 #this Module is used on Windows Servers without Powershell 5.x
+#https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-kile/6cfc7b50-11ed-4b4d-846d-6f08f0812919
 
 $EncTypes = @{
         "DES-CBC-CRC"             = 1
@@ -7,12 +8,15 @@ $EncTypes = @{
         "RC4-HMAC"                = 4
         "AES128-CTS-HMAC-SHA1-96" = 8
         "AES256-CTS-HMAC-SHA1-96" = 16
+        "FAST_Supported"          = 65536
+        "CompoundIdentity"        = 131072
+        "Claims_Supported"        = 262144
+        "Sid_Compression_Disabled"  = 524288
         }
-$KRBflags = [System.Collections.ArrayList]@()
+
 function enumerateKrb ([int]$EncType)
 {
-        
-
+        $KRBflags = [System.Collections.ArrayList]@()
         foreach ($etype in ($EncTypes.GetEnumerator() | Sort-Object -Property Value ))
         { if (($EncType -band $etype.Value) -ne 0) { $KRBflags.Add($etype.Key.ToString()) |out-null } }
         return $KRBflags -join " | "
--- helpermodules/krbtype_enum_v5.psm1
+++ helpermodules/krbtype_enum_v5.psm1
@@ -1,6 +1,6 @@
-﻿#HelperModule for Kerberos EncryptionType enumeration
+#HelperModule for Kerberos EncryptionType enumeration
 #this Module is used on Windows Servers with Powershell 5.x
-
+#https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-kile/6cfc7b50-11ed-4b4d-846d-6f08f0812919
 
 
 [flags()] Enum EncTypes
@@ -10,8 +10,12 @@
         RC4_HMAC                = 0x04
         AES128_CTS_HMAC_SHA1_96 = 0x08
         AES256_CTS_HMAC_SHA1_96 = 0x10
+        FAST_Supported          = 0x10000
+        CompoundIdentity        = 0x20000
+        Claims_Supported        = 0x40000
+        Sid_Compression_Disabled  = 0x80000
 }
 function enumerateKrb ([int]$EncType)
 {
-  return [regex]::replace(([EncTypes]$EncType), ", "," | ")}
-
+  return [regex]::replace(([EncTypes]$EncType), ", "," | ")
+}
